403edfc7c94f963dcfbbbdebe98a17f01d4628ee
heedong.kang <heedong.kang@lemonhc.com>
2021-10-12 17:41:13 +0900

    Initial version of blog generated by generator-jhipster@7.3.0

---
+++ blog/src/main/java/com/example/blog/repository/TagRepository.java
@@ -0,0 +1,12 @@
+package com.example.blog.repository;
+
+import com.example.blog.domain.Tag;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data SQL repository for the Tag entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {}
