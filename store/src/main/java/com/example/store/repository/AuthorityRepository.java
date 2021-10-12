163cc1baff6bc698232bc2284e24f406e30d702e
heedong.kang <heedong.kang@lemonhc.com>
2021-10-12 10:25:44 +0900

    Initial version of store generated by generator-jhipster@7.3.0

---
+++ store/src/main/java/com/example/store/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.example.store.repository;
+
+import com.example.store.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
